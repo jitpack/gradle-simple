49effea98e169baed063540909ea5cf26b3a101d
Roger Hu <rogerh@squareup.com>
2021-05-11 22:36:02 -0700

    Test with Gradle

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,13 @@
 
-public class Hello {
-  
-  public static String GREETING = "Hello world!";
-  
-  public static void main(String []args) {
-    
-  }  
-}
+import io.grpc.CallOptions;
 
+public class Hello {
+
+  public static String GREETING = "Hello world!";
+
+  public static void main(String []args) {
+
+      CallOptions bla = CallOptions.DEFAULT;
+      System.out.println(bla);
+  }
+}
