f13fd86520fcf9827e4335adeeaffc58d1d91118
Kavitha S <kavitha.s@thoughtworks.com>
2023-04-04 13:59:02 +0530

    BAH-2816 | refactor. api to read json body and response

---
+++ src/main/java/org/bahmni/sms/model/SMSContract.java
@@ -0,0 +1,22 @@
+package org.bahmni.sms.model;
+
+public class SMSContract {
+    private String phoneNumber;
+    private String message;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
